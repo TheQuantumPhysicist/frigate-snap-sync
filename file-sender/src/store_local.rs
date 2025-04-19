484ac87be70949516802f4f6a77964774cddcd30
Samer Afach <info@afach.de>
2025-04-19 14:18:49 +0400

    Make local store functions async

--- file-sender/src/store_local.rs
+++ file-sender/src/store_local.rs
@@ -1,7 +1,7 @@
-use std::fs;
 use std::path::Path;
 use std::path::PathBuf;
 use std::sync::Arc;
+use tokio::fs;
 
 use anyhow::Context;
 use async_trait::async_trait;
@@ -46,20 +46,28 @@ impl StoreDestination for LocalStore {
     async fn ls(&self, path: &Path) -> Result<Vec<PathBuf>, Self::Error> {
         let full_path = self.resolve(&path);
         tracing::debug!("Calling 'ls' on path: {}", full_path.display());
-        fs::read_dir(full_path)?
-            .map(|res| res.map(|e| e.file_name().into()))
-            .collect::<Result<_, std::io::Error>>()
-            .map_err(Into::into)
+
+        let mut entries = Vec::new();
+
+        let mut dir_read = fs::read_dir(full_path).await?;
+
+        while let Some(entry) = dir_read.next_entry().await? {
+            entries.push(entry.file_name().into());
+        }
+
+        Ok(entries)
     }
 
     async fn del_file(&self, path: &Path) -> Result<(), Self::Error> {
         let full_path = self.resolve(&path);
         tracing::debug!("Calling 'del_file' on path: {}", full_path.display());
-        fs::remove_file(full_path).map_err(Into::into)
+        fs::remove_file(full_path).await.map_err(Into::into)
     }
 
     async fn mkdir_p(&self, path: &Path) -> Result<(), Self::Error> {
-        fs::create_dir_all(self.resolve(&path)).map_err(Into::into)
+        fs::create_dir_all(self.resolve(&path))
+            .await
+            .map_err(Into::into)
     }
 
     async fn put(&self, from: &Path, to: &Path) -> Result<(), Self::Error> {
@@ -69,7 +77,10 @@ impl StoreDestination for LocalStore {
             from.display(),
             to_path.display()
         );
-        fs::copy(from, to_path).map(|_| ()).map_err(Into::into)
+        fs::copy(from, to_path)
+            .await
+            .map(|_| ())
+            .map_err(Into::into)
     }
 
     async fn put_from_memory(&self, from: &[u8], to: &Path) -> Result<(), Self::Error> {
@@ -79,7 +90,7 @@ impl StoreDestination for LocalStore {
             from.len(),
             to_path.display()
         );
-        Ok(fs::write(to_path, from)?)
+        Ok(fs::write(to_path, from).await?)
     }
 
     async fn dir_exists(&self, path: &Path) -> Result<bool, Self::Error> {
