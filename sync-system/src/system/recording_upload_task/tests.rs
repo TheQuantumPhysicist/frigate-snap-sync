b6609727d3f0cbee0007e195ca1e24c28a2d1890
Samer Afach <info@afach.de>
2025-04-28 21:47:35 +0400

    Add test that RecordingTaskHandler will never finish without stop

--- sync-system/src/system/recording_upload_task/tests.rs
+++ sync-system/src/system/recording_upload_task/tests.rs
@@ -1,8 +1,7 @@
+use super::RecordingTaskHandler;
 use crate::{
     config::PathDescriptors, system::recording_upload_task::RecordingsUploadTaskHandlerCommand,
 };
-
-use super::RecordingTaskHandler;
 use file_sender::{make_inmemory_filesystem, path_descriptor::PathDescriptor};
 use frigate_api_caller::{config::FrigateApiConfig, traits::FrigateApi};
 use mocks::frigate_api::make_frigate_client_mock;
@@ -55,6 +54,17 @@ async fn get_task_count(
         .unwrap();
 
     size_receiver.await.unwrap()
+}
+
+async fn assert_not_finished_for(
+    handle: &tokio::task::JoinHandle<impl Send + 'static>,
+    duration: std::time::Duration,
+) {
+    let start = tokio::time::Instant::now();
+    while start.elapsed() < duration {
+        assert!(!handle.is_finished(), "Task finished too early!");
+        tokio::time::sleep(std::time::Duration::from_millis(10)).await;
+    }
 }
 
 #[tokio::test]
@@ -199,6 +209,75 @@ async fn recordings_task_handler(random_seed: Seed) {
 
         // Now after the end event, the task should be evicted
         assert_eq!(get_task_count(&cmd_sender).await, 0);
+    }
+
+    // stop and shutdown
+    {
+        cmd_sender
+            .send(RecordingsUploadTaskHandlerCommand::Stop)
+            .unwrap();
+
+        task_handle.await.unwrap();
+    }
+}
+
+#[tokio::test]
+#[rstest]
+async fn recordings_task_handler_shutdown(random_seed: Seed) {
+    let mut rng = make_seedable_rng(random_seed);
+
+    let (cmd_sender, cmd_receiver) = tokio::sync::mpsc::unbounded_channel();
+
+    let frigate_config = FrigateApiConfig {
+        frigate_api_base_url: "http://someurl.com:5000/".to_string(),
+        frigate_api_proxy: None,
+    };
+
+    let path_descriptors = PathDescriptors {
+        path_descriptors: Arc::new(vec![Arc::new(PathDescriptor::Local(
+            "/home/data/".to_string().into(),
+        ))]),
+    };
+
+    let expected_file_content = Arc::new(Mutex::new(gen_random_bytes(&mut rng, 100..1000)));
+    let expected_file_content_inner = expected_file_content.clone();
+
+    // Prepare the file sender
+    let file_sender = make_inmemory_filesystem();
+    let file_sender_inner = file_sender.clone();
+
+    // Prepare the API mock
+    let mut frigate_api_mock = make_frigate_client_mock();
+    frigate_api_mock
+        .expect_recording_clip()
+        .returning(move |_, _, _| {
+            Ok(Some(
+                expected_file_content_inner.clone().lock().unwrap().clone(),
+            ))
+        });
+    let frigate_api_mock: Arc<dyn FrigateApi> = Arc::new(frigate_api_mock);
+    let frigate_api_maker = Arc::new(move |_: &FrigateApiConfig| Ok(frigate_api_mock.clone()));
+
+    let file_sender_maker = Arc::new(move |_: &Arc<PathDescriptor>| Ok(file_sender_inner.clone()));
+
+    let task = RecordingTaskHandler::new(
+        cmd_receiver,
+        Arc::new(frigate_config),
+        frigate_api_maker,
+        file_sender_maker,
+        path_descriptors,
+    );
+
+    let task_handle = tokio::task::spawn(task.run());
+
+    assert_eq!(get_task_count(&cmd_sender).await, 0);
+
+    let wait_time = std::time::Duration::from_secs(5);
+
+    // Without the stop signal, it won't stop
+    {
+        assert_not_finished_for(&task_handle, wait_time).await;
+        assert!(!task_handle.is_finished());
     }
 
     // stop and shutdown
